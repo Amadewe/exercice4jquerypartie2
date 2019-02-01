e88ae912c9e0e992dd9fc3cef10c585c0d1d321b
Amadewe <dewevre.amandine@gmail.com>
2019-02-01 21:52:32 +0100

    Fin de l'exercice 4

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Au clic sur un bouton de couleur, modifier la couleur du texte de la div.
+$('#green').click(function() {
+  $('#text').css('color', 'green');
+});
+$('#red').click(function() {
+  $('#text').css('color', 'red');
+});
+$('#blue').click(function() {
+  $('#text').css('color', 'blue');
+});
